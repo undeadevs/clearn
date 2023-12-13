8d51690281040fd7a4b98da9cec898ddf2d5d6f7
Ilham <jangantanyaaku14@gmail.com>
2023-12-13 13:00:24 +0800

    Add shebang bash

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+
 gcc -Wall -Wextra -Werror -O2 -std=c99 -pedantic guessnum.c -lm -o guessnum.out
 gcc -Wall -Wextra -Werror -O2 -std=c99 -pedantic dijkstra.c -lm -o dijkstra.out
